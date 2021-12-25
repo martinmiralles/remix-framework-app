32a6508b6885ab8bd3d4e8029076cb2e6e9dad49
Martin Miralles <martinmiralles66@gmail.com>
2021-12-25 17:41:13 -0500

    Installed bcrypt, created session cookies on login

--- app/routes/auth/login.jsx
+++ app/routes/auth/login.jsx
@@ -1,5 +1,6 @@
 import { useActionData, json, redirect } from "remix";
 import { db } from "~/utils/db.server";
+import { login, createUserSession } from "~/utils/session.server";
 
 function validateUsername(username) {
   if (typeof username !== "string" || username.length < 3) {
@@ -39,8 +40,18 @@ export const action = async ({ request }) => {
   switch (loginType) {
     case "login": {
       //  Find User
+      const user = await login({ username, password });
+
       //  Check user
+      if (!user) {
+        return badRequest({
+          fields,
+          fieldErrors: { username: "Invalid Credentials" },
+        });
+      }
+
       //  Create user session
+      return createUserSession(user.id, "/posts");
     }
 
     case "register": {
